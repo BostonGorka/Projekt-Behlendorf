c17b8378c1d815befc46e8c30637c93fe533322d
BostonGorka <eric.vilhelmsson@elev.ga.lbs.se>
2025-01-22 14:51:01 +0100

    2025-01-22

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import http from 'http';
 import { MongoClient } from 'mongodb';
-import { getRequestBody, cleanupHTMLOutput} from './utilities.js';
+import { getRequestBody, cleanupHTMLOutput } from './utilities.js';
 import fs from 'fs/promises';
 import { handleProfilesRoute } from './routes/post-route.js';
 
@@ -12,15 +12,15 @@ export let dbo = dbConn.db(process.env.MONGODB_DATABASE_NAME);
 
 async function handleRequest(request, response) {
 
-	let url = new URL(request.url, 'http://' + request.headers.host);
-	let path = url.pathname;
-	let pathSegments = path.split('/').filter(function (segment) {
-		if (segment === '' || segment === '..') {
-			return false;
-		} else {
-			return true;
-		}
-	});
+    let url = new URL(request.url, 'http://' + request.headers.host);
+    let path = url.pathname;
+    let pathSegments = path.split('/').filter(function (segment) {
+        if (segment === '' || segment === '..') {
+            return false;
+        } else {
+            return true;
+        }
+    });
 
     let nextSegment = pathSegments.shift();
 
@@ -36,7 +36,7 @@ async function handleRequest(request, response) {
 
         let profileString = '';
 
-       
+
         for (let i = 0; i < documents.length; i++) {
             profileString += '<li><a href="/showcasepost/' + cleanupHTMLOutput(documents[i]._id.toString()) + '">' + cleanupHTMLOutput(documents[i].title) + ')</a></li>';
         }
@@ -72,18 +72,30 @@ async function handleRequest(request, response) {
         return;
     }
     if (nextSegment === 'managepost') {
-        if (request.method !== 'POST') {
-            response.writeHead(405, { 'Content-Type': 'text/plain' });
-            response.write('405 Method Not Allowed');
+        if (request.method === 'GET') {
+            let template = (await fs.readFile('templates/managepost.blogg')).toString();
+           //
+            template = template.replaceAll('%{userName}%', cleanupHTMLOutput(profileDocument.userName));
+            template = template.replaceAll('%{title}%', cleanupHTMLOutput(profileDocument.title));
+            template = template.replaceAll('%{breadText}%', cleanupHTMLOutput(profileDocument.breadText));
+
+
+            response.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' });
+            response.write(template);
             response.end();
             return;
-        }
-        let template = (await fs.readFile('templates/create-post.blogg')).toString();
 
-        response.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' });
-        response.write(template);
-        response.end();
-        return;
+        }
+        if (request.method === 'POST') {
+            let template = (await fs.readFile('templates/managepost.blogg')).toString();
+
+            response.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' });
+            response.write(template);
+            response.end();
+            return;
+
+        }
+
 
     }
 }
